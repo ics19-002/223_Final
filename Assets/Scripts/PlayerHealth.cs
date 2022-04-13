de02ab7b8c701cbab6590d566ec6562a2bc03f1f
ics19-002 <ICS19-002@camosun.bc.ca>
2022-04-13 13:53:54 -0700

    Player takes damage on impact with enemies

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,31 +5,28 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
 
-    [SerializeField] public float health, maxHealth;
+    public float health, maxHealth;
     [SerializeField] private Health healthBar;
     public void TakeDamage()
     {
-        // Use your own damage handling code, or this example one.
         health -= 0.1f;
         healthBar.UpdateHealthBar();
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        { 
-            TakeDamage();
-        }
     }
 
     private void Start()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    TakeDamage();
-        //}
         maxHealth = 1f;
         health = maxHealth;
-        TakeDamage();
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player hit by " + other.tag);
+        if (other.tag == "Enemy")
+        {
+            
+            TakeDamage();
+            
+        }
     }
 }
